76fa16c58d69b8809fcb152fc5f7b5583037dbb5
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-10-13 11:22:08 -0500

    Add nvim initial motion docs

---
+++ nvim/docs/motions.tsx
@@ -0,0 +1,16 @@
+/*
+
+# Comment out a block of code
+
+- Enter visual block mode:
+  - `Ctrl + v`
+- Select lines:
+  - `hjkl`
+- Enter insert mode:
+  - `Shift + i`
+- Type the comment character for the specific language:
+  - `// for javascript, # for python, etc`
+- Insert character on all selected lines:
+  - Press `Esc`
+
+*/
