f4a9ea9022765c5e8ccb83f70fd7d16c24ff0af7
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-10-20 11:14:23 -0500

    Add nvim todo-comments plugin

--- nvim/lua/dylan/plugins/trouble.lua
+++ nvim/lua/dylan/plugins/trouble.lua
@@ -1,6 +1,9 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim"
+  },
   opts = {
     focus = true,
   },
