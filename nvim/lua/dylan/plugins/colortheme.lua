e07d4223474f18133f917a5f5fc1ac36507c3ef2
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-10-23 16:57:35 -0500

    Add keymap to toggle color theme

--- nvim/lua/dylan/plugins/colortheme.lua
+++ nvim/lua/dylan/plugins/colortheme.lua
@@ -9,7 +9,12 @@ return {
         italic_comments = false,
         borderless_telescope = false,
       })
-     vim.cmd.colorscheme "cyberdream"
+      vim.cmd.colorscheme "cyberdream"
+      -- Toggle color theme.
+      vim.api.nvim_set_keymap("n", "<leader>ct", ":CyberdreamToggleMode<CR>", {
+        noremap = true,
+        silent = true,
+      })
     end
   },
 }
