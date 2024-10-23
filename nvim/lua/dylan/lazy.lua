c2f6271831f6a1bc534822d68eb069e3dcca85e6
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-10-23 09:40:06 -0500

    Add lazy install

---
+++ nvim/lua/dylan/lazy.lua
@@ -0,0 +1,29 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--branch=stable",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+-- Plugins
+{
+},
+-- Options
+{
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	}
+})
