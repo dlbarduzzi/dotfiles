f9e223daf61d50ef140f444b0559ad3f1060ac66
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-02 12:01:50 -0600

    Update wezterm configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,8 +9,8 @@ config.enable_tab_bar = false
 
 config.colors = require("cyberdream")
 
-config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", { weight = "DemiBold" })
-config.font_size = 16
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", { weight = "Regular" })
+config.font_size = 17
 config.line_height = 1.4
 
 config.window_decorations = "RESIZE"
