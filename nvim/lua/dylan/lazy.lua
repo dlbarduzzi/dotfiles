c24bdd08f97a6b41671b67b17f1ca80cbc174de7
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-24 06:52:01 -0600

    Add neovim mini plugin

--- nvim/lua/dylan/lazy.lua
+++ nvim/lua/dylan/lazy.lua
@@ -15,6 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { import = "dylan.plugins.colorscheme" },
+  { import = "dylan.plugins.mini" },
 }, {
   checker = {
     notify = false,
