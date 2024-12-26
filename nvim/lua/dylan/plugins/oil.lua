afd547d9f6843facebb054ac6b8c7ceb13c29834
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-26 09:28:10 -0600

    Update oil to show hidden files

--- nvim/lua/dylan/plugins/oil.lua
+++ nvim/lua/dylan/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
     vim.keymap.set("n", "<leader>ee", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
