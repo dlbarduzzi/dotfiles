7dc904a302d0627ac86798d4385ca9b2a6b426e8
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-27 10:35:16 -0600

    Add luasnip neovim completion plugin

--- nvim/lua/dylan/plugins/completion.lua
+++ nvim/lua/dylan/plugins/completion.lua
@@ -1,13 +1,42 @@
 return {
   "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+    },
+  },
   version = "*",
   opts = {
     keymap = { preset = "default" },
     appearance = {
       nerd_font_variant = "mono",
-      use_nvim_cmp_as_default = true
+      use_nvim_cmp_as_default = true,
+    },
+    snippets = {
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet.body)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
+    sources = {
+      default = { "lsp", "path", "luasnip", "buffer" },
     },
     signature = { enabled = true },
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+    },
   },
 }
