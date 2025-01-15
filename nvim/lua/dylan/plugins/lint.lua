f2fe6a9c3e8990c24624466e08f6574b08433467
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-01-15 12:56:51 -0600

    Revert neovim lint plugin to show messages only on hover

--- nvim/lua/dylan/plugins/lint.lua
+++ nvim/lua/dylan/plugins/lint.lua
@@ -28,19 +28,19 @@ return {
 
     -- This block together simply highlights lint warning/errors but do no show messages
     -- by default. Once you hover over the text, then the warn/error will show up.
-    vim.diagnostic.config({ virtual_text = false, underline = true })
-    vim.api.nvim_create_autocmd("CursorHold", {
-      callback = function()
-        vim.diagnostic.open_float(nil, {
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-          prefix = "",
-          source = "always",
-          border = "single",
-          focusable = false,
-        })
-      end,
-      desc = "Show lint diagnostics on hover",
-    })
+    --vim.diagnostic.config({ virtual_text = false, underline = true })
+    --vim.api.nvim_create_autocmd("CursorHold", {
+    --  callback = function()
+    --    vim.diagnostic.open_float(nil, {
+    --      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+    --      prefix = "",
+    --      source = "always",
+    --      border = "single",
+    --      focusable = false,
+    --    })
+    --  end,
+    --  desc = "Show lint diagnostics on hover",
+    --})
 
     vim.keymap.set("n", "<leader>li", function()
       lint.try_lint()
