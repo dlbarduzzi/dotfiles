7753b9e73d525d7c0c60819354a2cdb9e2ef9cc0
Dylan Barduzzi <dlbarduzz@gmail.com>
2025-05-14 09:29:09 -0500

    Update color theme

--- nvim/lua/dylan/plugins/colorscheme.lua
+++ nvim/lua/dylan/plugins/colorscheme.lua
@@ -1,17 +1,48 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     enabled = true,
+--     priority = 1000,
+--     config = function()
+--       require("tokyonight").setup({
+--         style = "moon",
+--         styles = {
+--           keywords = { italic = false },
+--         },
+--       })
+--       vim.cmd.colorscheme("tokyonight")
+--     end
+--   },
+-- }
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    enabled = true,
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        style = "moon",
-        styles = {
-          keywords = { italic = false },
+  "loctvl842/monokai-pro.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("monokai-pro").setup({
+      filter = "machine",
+      background_clear = {
+        "nvim-tree",
+        "bufferline",
+        "telescope",
+        "float_win",
+        "which-key",
+      },
+      plugins = {
+        bufferline = {
+          underline_selected = false,
+          underline_visible = false,
         },
-      })
-      vim.cmd.colorscheme("tokyonight")
-    end
-  },
+      },
+      overridePalette = function(filter)
+        return {
+          -- dark2 = "#1d2528",
+          --    dark1 = "#1d2528",
+          background = "#161b1e",
+        }
+      end,
+    })
+    vim.cmd([[colorscheme monokai-pro]])
+  end,
 }
